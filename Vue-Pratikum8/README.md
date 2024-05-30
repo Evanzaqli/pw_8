eb7b65e627581a2452ccb1e9f0cdff0baefd2c08
Evan Zaqli <evanzaqli1@gmail.com>
2024-05-30 17:07:21 +0700

    commit gambar

--- Vue-Pratikum8/README.md
+++ Vue-Pratikum8/README.md
@@ -1,4 +1,4 @@
-# vue-project
+#  Vue-Pratikum8
 
 This template should help get you started developing with Vue 3 in Vite.
 
---
+++ Vue-Pratikum8/src/components/ArtikelHome.vue
@@ -0,0 +1,10 @@
+<script setup>
+
+</script>
+
+<template>
+    <img alt="gambar" class="gambar" src="@/assets/image.png" width="125" height="125" />
+    <h1>Ini adalah komponen artikel ini</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet officiis veritatis cum eos, tempora sed placeat non temporibus molestiae optio ullam sint error. Eius, illo non iure soluta architecto nostrum?</p>
+    
+</template>
